07a626895be2f3809d4798a32dac7c59400e198b
Mario Thompson <mariothompson181998@gmail.com>
2022-11-14 23:16:46 -0500

    Edited php file

--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -9,9 +9,53 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// ?>
+// <ul>
+// <?php foreach ($results as $row): ?>
+//   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+// <?php endforeach; ?>
+// </ul>
+
+<?php
+  echo $_GET['country'];
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($countries as $country): ?>
+    <tr>
+      <td><?= $country['name']; ?></td>
+      <td><?= $country['continent']; ?></td>
+      <td><?= $country['independence_year']; ?></td>
+      <td><?= $country['head_of_state']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($cities as $city): ?>
+    <tr>
+      <td><?= $city['name']; ?></td>
+      <td><?= $city['district']; ?></td>
+      <td><?= $city['population']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
